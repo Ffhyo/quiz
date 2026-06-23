a84b365ac1c6b038aa6159f47b314853b047ae0d
Ffhyo <ffhyo38@gmail.com>
2026-06-23 20:05:41 +0545

    update status

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -12,7 +12,7 @@ export function Auth( ){
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://quizbackend-yo87.onrender.com/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
